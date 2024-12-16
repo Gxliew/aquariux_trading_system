3e3031638f6e2100b479402d114a15e29227c7ad
LiewGuanXiang <gxliew@hotmail.com>
2024-12-16 14:40:54 +0800

    project setup

---
+++ src/test/java/com/example/aquariux_test/AquariuxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquariux_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariuxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
