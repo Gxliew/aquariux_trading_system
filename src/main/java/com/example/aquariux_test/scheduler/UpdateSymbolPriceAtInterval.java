63d1250ee4b2b30396fb3ec0fc130421151ebf32
LiewGuanXiang <gxliew@hotmail.com>
2024-12-16 17:19:07 +0800

    setup scheduler

---
+++ src/main/java/com/example/aquariux_test/scheduler/UpdateSymbolPriceAtInterval.java
@@ -0,0 +1,18 @@
+package com.example.aquariux_test.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.aquariux_test.service.SymbolPriceService;
+
+@Component
+public class UpdateSymbolPriceAtInterval {
+    @Autowired
+    private SymbolPriceService symbolPriceService;
+
+    @Scheduled(fixedDelay = 10000)
+    public void runTask() {
+        symbolPriceService.updateSymbolPrice();
+    }
+}
